78dfb935975e7cca1836e9a2f0af0c730865ad1a
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-02-15 22:40:01 +0300

    cart closer fix

--- gulpfile.js
+++ gulpfile.js
@@ -21,13 +21,14 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(gulp.dest("src/css"))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
